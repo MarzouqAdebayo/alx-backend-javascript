1715ea27fa110f3e92d3bb8256bbdcb36e0cc799
MarzouqAdebayo <marzouqaadebayo@gmail.com>
2024-10-03 02:51:06 +0100

    Changed nothing, testing again

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -9,9 +9,9 @@ export default async function handleProfileSignup(
   return Promise
     .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
     .then((res) => (
-      res.map((o) => ({
-        status: o.status,
-        value: o.status === 'fulfilled' ? o.value : String(o.reason),
+      res.map((result) => ({
+        status: result.status,
+        value: result.status === 'fulfilled' ? result.value : String(result.reason),
       }))
     ));
 }
