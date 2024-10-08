47e38b2ccf5cf2b91a91f371cbea9bd961ba4e05
MarzouqAdebayo <marzouqaadebayo@gmail.com>
2024-10-08 04:50:18 +0100

    Testing again

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,11 +1,8 @@
 export default function cleanSet(set, startString) {
   const arr = [];
-  if (set instanceof Set && typeof startString === 'string') {
-    for (const str of set) {
-      if (
-        startString.length
-        && str.startsWith(startString)
-      ) {
+  if (set && set instanceof Set && typeof startString === 'string') {
+    for (const str of set.value()) {
+      if (startString.length && str.startsWith(startString)) {
         arr.push(str.slice(startString.length));
       }
     }
