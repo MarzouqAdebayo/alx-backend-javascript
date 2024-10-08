add69e2241876bce5035da234e7fe1e7ec86f10e
MarzouqAdebayo <marzouqaadebayo@gmail.com>
2024-10-08 04:42:33 +0100

    Added type guards to task 8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,8 +1,14 @@
 export default function cleanSet(set, startString) {
   const arr = [];
-  for (const str of set) {
-    if (startString && str.startsWith(startString)) {
-      arr.push(str.slice(startString.length));
+  if (set instanceof Set) {
+    for (const str of set) {
+      if (
+        startString
+        && typeof startString === 'string'
+        && str.startsWith(startString)
+      ) {
+        arr.push(str.slice(startString.length));
+      }
     }
   }
   return arr.join('-');
