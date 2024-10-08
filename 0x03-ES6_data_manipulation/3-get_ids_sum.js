af34026e52af4f72cd4634cc5c0f429e962bfe24
MarzouqAdebayo <marzouqaadebayo@gmail.com>
2024-10-08 02:32:46 +0100

    Fixed eslint error in task 3 file

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -2,7 +2,5 @@ export default function getStudentIdsSum(listOfStudents) {
   if (!Array.isArray(listOfStudents)) {
     return [];
   }
-  return listOfStudents.reduce((prevVal) => {
-    return prevVal + 1;
-  }, 0);
+  return listOfStudents.reduce((prevVal) => prevVal + 1, 0);
 }
