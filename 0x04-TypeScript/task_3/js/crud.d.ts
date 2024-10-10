2cdb0972a51e3be8138d3707a96e902e4940512f
MarzouqAdebayo <marzouqaadebayo@gmail.com>
2024-10-10 03:41:10 +0100

    Completed task 3

---
+++ 0x04-TypeScript/task_3/js/crud.d.ts
@@ -0,0 +1,5 @@
+import { RowID, RowElement } from "./interface";
+
+declare function insertRow(row: RowElement): number;
+declare function deleteRow(rowID: RowID): void;
+declare function updateRow(rowID: RowID, row: RowElement): RowID;
