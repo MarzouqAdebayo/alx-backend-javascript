a09740e2b56a592c756734370a0d6a65612cef48
MarzouqAdebayo <marzouqaadebayo@gmail.com>
2024-11-28 07:26:00 +0100

    :bug: Fixed bugs in tasks 1 and 2

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,25 +1,42 @@
 const fs = require('fs');
 
 /**
+ * @typedef {Object.<string, string[]>} StringArrayMap
+ * */
+
+/**
  * countStudents - Counts the number of students in a csv db
  * @param {String} path - path to csv file
  */
 function countStudents(path = './database.csv') {
   try {
-    const data = fs.readFileSync(path);
-    const lines = String(data).trim().split('\n');
+    const lines = fs.readFileSync(path, 'utf-8').toString().trim().split('\n');
     console.log('Number of students: ', lines.length - 1);
+    const allFields = lines[0].split(',');
+    const nameIdx = allFields.indexOf('firstname');
+    const fieldIdx = allFields.indexOf('field');
     const csvArr = lines.slice(1).map((line) => line.split(','));
-    const csStudents = csvArr.filter((student) => student.includes('CS'));
-    const csStudentsNames = csStudents.map((student) => student[0]);
-    console.log(
-      `Number of students in CS: ${csStudents.length}. List: ${csStudentsNames.join(', ')}`,
-    );
-    const sweStudents = csvArr.filter((student) => student.includes('SWE'));
-    const sweStudentsNames = sweStudents.map((student) => student[0]);
-    console.log(
-      `Number of students in SWE: ${sweStudents.length}. List: ${sweStudentsNames.join(', ')}`,
-    );
+    /** @type {string[]} */
+    const fields = [];
+    /** @type {StringArrayMap} */
+    const groupByField = {};
+    csvArr.forEach((student) => {
+      const studentField = student[fieldIdx];
+      const studentName = student[nameIdx];
+      if (!fields.includes(studentField)) {
+        fields.push(studentField);
+      }
+      if (!groupByField[studentField]) {
+        groupByField[studentField] = [studentName];
+      } else {
+        groupByField[studentField].push(studentName);
+      }
+    });
+    for (const [field, names] of Object.entries(groupByField)) {
+      console.log(
+        `Number of students in ${field}: ${names.length}. List: ${names.join(', ')}`,
+      );
+    }
   } catch (error) {
     throw new Error('Cannot load the database');
   }
