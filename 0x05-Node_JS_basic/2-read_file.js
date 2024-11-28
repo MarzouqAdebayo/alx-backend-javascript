24a77867302ff86a0566dc93511ff64667afb2c4
MarzouqAdebayo <marzouqaadebayo@gmail.com>
2024-11-28 07:32:39 +0100

    :bug: task 2

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -8,30 +8,25 @@ const fs = require('fs');
  * countStudents - Counts the number of students in a csv db
  * @param {String} path - path to csv file
  */
-function countStudents(path = './database.csv') {
+function countStudents(path) {
   try {
     const lines = fs.readFileSync(path, 'utf-8').toString().trim().split('\n');
-    console.log('Number of students: ', lines.length - 1);
     const allFields = lines[0].split(',');
     const nameIdx = allFields.indexOf('firstname');
     const fieldIdx = allFields.indexOf('field');
     const csvArr = lines.slice(1).map((line) => line.split(','));
-    /** @type {string[]} */
-    const fields = [];
     /** @type {StringArrayMap} */
     const groupByField = {};
     csvArr.forEach((student) => {
       const studentField = student[fieldIdx];
       const studentName = student[nameIdx];
-      if (!fields.includes(studentField)) {
-        fields.push(studentField);
-      }
       if (!groupByField[studentField]) {
         groupByField[studentField] = [studentName];
       } else {
         groupByField[studentField].push(studentName);
       }
     });
+    console.log('Number of students: ', csvArr.length);
     for (const [field, names] of Object.entries(groupByField)) {
       console.log(
         `Number of students in ${field}: ${names.length}. List: ${names.join(', ')}`,
