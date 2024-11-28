15ec0aa84c306680b621336eae0cb33e35df29c0
MarzouqAdebayo <marzouqaadebayo@gmail.com>
2024-11-28 05:51:33 +0100

    :sparkles: initial commit

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,9 @@
+/**
+ * Displays a arg message in STDOUT
+ * @param {String} arg - message to displayMessage
+ */
+function displayMessage(arg) {
+  console.log(arg);
+}
+
+module.exports = displayMessage;
