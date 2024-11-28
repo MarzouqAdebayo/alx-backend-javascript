a9f992b1dfa457098b9eb4a507f9627d837cb929
MarzouqAdebayo <marzouqaadebayo@gmail.com>
2024-11-28 06:51:13 +0100

    :bug: fix error in task 2

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,27 +1,27 @@
-const fs = require("fs");
+const fs = require('fs');
 
 /**
  * countStudents - Counts the number of students in a csv db
  * @param {String} path - path to csv file
  */
-function countStudents(path = "./database.csv") {
+function countStudents(path = './database.csv') {
   try {
     const data = fs.readFileSync(path);
-    const lines = String(data).split("\n");
-    console.log("Number of students: ", lines.length - 2);
-    const csvArr = lines.slice(1).map((line) => line.split(","));
-    const csStudents = csvArr.filter((student) => student.includes("CS"));
+    const lines = String(data).trim().split('\n');
+    console.log('Number of students: ', lines.length - 1);
+    const csvArr = lines.slice(1).map((line) => line.split(','));
+    const csStudents = csvArr.filter((student) => student.includes('CS'));
     const csStudentsNames = csStudents.map((student) => student[0]);
     console.log(
-      `Number of students in CS: ${csStudents.length}. List: ${csStudentsNames.join(", ")}`,
+      `Number of students in CS: ${csStudents.length}. List: ${csStudentsNames.join(', ')}`,
     );
-    const sweStudents = csvArr.filter((student) => student.includes("SWE"));
+    const sweStudents = csvArr.filter((student) => student.includes('SWE'));
     const sweStudentsNames = sweStudents.map((student) => student[0]);
     console.log(
-      `Number of students in CS: ${sweStudents.length}. List: ${sweStudentsNames.join(", ")}`,
+      `Number of students in CS: ${sweStudents.length}. List: ${sweStudentsNames.join(', ')}`,
     );
   } catch (error) {
-    throw new Error("Cannot load the database");
+    throw new Error('Cannot load the database');
   }
 }
 
