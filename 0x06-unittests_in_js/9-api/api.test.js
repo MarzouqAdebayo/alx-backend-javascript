d543ea1baee833dcf809dae241b893f3585efd9d
MarzouqAdebayo <marzouqaadebayo@gmail.com>
2024-11-30 19:21:00 +0100

    Fixed 9 too

--- 0x06-unittests_in_js/9-api/api.test.js
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -1,6 +1,16 @@
 const request = require('request');
 const { expect } = require('chai');
 
+describe('Simple API intergration test for Index page', () => {
+  it('/ route return correct response', (done) => {
+    request.get('http://localhost:7865', (_, res, body) => {
+      expect(res.statusCode).to.be.equal(200);
+      expect(body).to.be.equal('Welcome to the payment system');
+      done();
+    });
+  });
+});
+
 describe('Simple API intergration test for /cart/:id', () => {
   it('/cart/:id route return correct response', (done) => {
     request.get('http://localhost:7865/cart/2', (_, res, body) => {
