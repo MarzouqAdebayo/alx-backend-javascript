a29ca919daf449dab2e54d9d1b01699b57fc0724
MarzouqAdebayo <marzouqaadebayo@gmail.com>
2024-11-30 13:34:02 +0100

    :sparkles: completed task 1

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,21 +1,23 @@
 const assert = require('assert');
 const calculateNumber = require('./0-calcul');
 
-describe('calculateNumber', function () {
-  it('it shoud round first before sum and return correct value', function () {
+describe('calculateNumber', () => {
+  it('shoud round first before sum and return correct value', () => {
     const a = 2.1111333;
     const b = 3.5000555;
     const want = Math.round(a) + Math.round(b);
     const got = calculateNumber(a, b);
-    assert.equal(want, got);
+    assert.strictEqual(want, got);
   });
 
-  it('it shoud check if individual numbers are rounded first before sum', function () {
+  it('shoud check if individual numbers are rounded first before sum', () => {
     const a = 2.3;
     const b = 3.3;
-    const want = Math.round(a + b);
     const got = calculateNumber(a, b);
-    console.log(want, got);
-    assert.notEqual(want, got);
+    assert.strictEqual(got, 5);
+  });
+
+  it("rounding down a and b floating point fractional numbers with trailing 9's", () => {
+    assert.strictEqual(calculateNumber(2.499999, 3.499999), 5);
   });
 });
