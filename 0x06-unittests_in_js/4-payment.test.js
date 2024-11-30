f633b7df3fded05670ed9693c91e34fb72ce7aad
MarzouqAdebayo <marzouqaadebayo@gmail.com>
2024-11-30 16:38:44 +0100

    fixed bugs

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -1,14 +1,14 @@
 const { expect } = require('chai');
 const sinon = require('sinon');
 const Utils = require('./utils');
-const sendPaymentRequestToApi = require('./3-payment');
+const sendPaymentRequestToApi = require('./4-payment');
 
 describe('Send payment request to api', () => {
   it('sendPaymentRequestToApi calls calculateNumber exactly once with arguments', () => {
     const consoleSpy = sinon.spy(console);
     const stub = sinon.stub(Utils, 'calculateNumber');
-    stub.returns(10);
 
+    stub.returns(10);
     sendPaymentRequestToApi(100, 20);
     expect(stub.calledOnceWithExactly('SUM', 100, 200)).to.be.true;
     expect(consoleSpy.log.calledOnceWithExactly('The total is: 10')).to.be.true;
